5eb3b7dd969864063fff7cb6806b9e554a6e0eca
ravi-basrani <rahulbasranikhairthal@gmail.com>
2021-06-04 00:50:27 +0530

    address port number

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const nodemailer = require("nodemailer");
 const app = express();
 const cors = require("cors");
-
 const dotenv = require("dotenv");
+const port = process.env.PORT || 8000;
 
 dotenv.config("./.env");
 
 require("./db/conn");
 app.use(express.json());
 app.use(cors());
-// const User=require('./model/userSchema');
 app.use(require("./router/auth"));
-const middleware = (req, res, next) => {
-  next();
-};
 
-app.listen(process.env.PORT, () => {
-  console.log(`server running at port no ${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`server running at port no ${port}`);
 });
